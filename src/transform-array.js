5fdfd29f9efa3e8f54aa7f410779b074cf539444
seregaby2 <serega.by@inbox.ru>
2021-09-14 15:24:11 +0300

    feat: add solution task recursive-depth

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,29 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function transform(arr) {
+  if(!Array.isArray(arr)) {
+    throw Error("'arr' parameter must be an instance of the Array!");
+  }
+   let array = [];
+   for(let i = 0; i < arr. length; i++) {
+     if(arr[i] === '--discard-next') {
+       i ++
+     }
+     else if (arr[i] === '--discard-prev' && arr[i - 2] !== '--discard-next') {
+       array.pop()
+     }
+     else if (arr[i] === '--double-next' && arr[i + 2] !== undefined ) {
+       array.push(arr[i+1])
+       array.push(arr[i+1])
+       i++
+     }
+     else if (arr[i] === '--double-prev' && arr[i - 2] !== '--discard-next' && arr[i-1] !== undefined) {
+       array.push(arr[i-1])
+     }
+     else if (arr[i] !== '--discard-next' && arr[i] !== '--discard-prev' && arr[i] !== '--double-next' && arr[i] !== '--double-prev') {
+     array.push(arr[i])
+     }
+   }
+   return array;
+ }
