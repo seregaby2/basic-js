10bbd6fbc60418af027d2dfcd4871352f5c837d8
seregaby2 <serega.by@inbox.ru>
2021-09-15 19:37:06 +0300

    feat: add solution task st-extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,28 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  str = String(str);
+  //if(!options.addition) options.addition = '';
+  if (options.addition || options.addition === null) options.addition = String(options.addition);
+  if(!options.repeatTimes) options.repeatTimes = 1;
+  if(!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+  if(!options.separator) options.separator = '+';
+  if(!options.additionSeparator) options.additionSeparator = '|';
+  let arr = [];
+  let arrInside = [];
+  let result = '';
+  for(let i = 0; i < options.additionRepeatTimes; i++) {
+    arrInside.push(options.addition);
+    arrInside.push(options.additionSeparator)
+  }
+  arrInside.pop(options.additionSeparator)
+  for(let j = 0; j < options.repeatTimes; j++) {
+    arr.push(str)
+    arr.push(arrInside)
+    arr.push(options.separator)
+  }
+  arr.pop(options.separator)
+  result = arr.flat().join('');
+  return result
 }
