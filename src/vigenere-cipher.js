65f6d7271a5365a3a8e7660fcd9e7bf21b9ee139
seregaby2 <serega.by@inbox.ru>
2021-10-03 16:33:18 +0300

    feat: add few solution vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,10 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message,key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message,key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
   }
 }
